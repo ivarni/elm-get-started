5ddca40dea9b2f55a893923f574d46b9a4c32038
Ivar Nilsen <post@ivarnilsen.com>
2016-05-26 04:25:33 +0200

    3rd exercise

--- 3-forms.elm
+++ 3-forms.elm
@@ -1,7 +1,10 @@
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import String
+import Regex
+import String
 
 main =
     Html.beginnerProgram
@@ -14,13 +17,15 @@ main =
 
 type alias Model =
     { name : String
+    , age : String
     , password : String
     , passwordAgain : String
+    , submitted : Bool
     }
 
 model : Model
 model =
-    Model "" "" ""
+    Model "" "" "" "" False
 
 --Update
 
@@ -28,6 +33,8 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
+    | Submit
 
 update : Msg -> Model -> Model
 update action model =
@@ -35,11 +42,17 @@ update action model =
     Name name ->
       { model | name = name }
 
+    Age age ->
+      { model | age = age }
+
     Password password ->
       { model | password = password }
 
     PasswordAgain password ->
       { model | passwordAgain = password }
+
+    Submit ->
+      { model | submitted = True }
 
 --View
 
@@ -47,19 +60,28 @@ view : Model -> Html Msg
 view model =
     div []
         [ input [type' "text", placeholder "Name", onInput Name] []
+        , input [type' "number", placeholder "Age", onInput Age] []
         , input [type' "password", placeholder "Password", onInput Password] []
         , input [type' "password", placeholder "Re-enter password", onInput PasswordAgain] []
+        , div [] [
+          input [type' "submit", onClick Submit] []
+        ]
         , viewValidation model
         ]
 
 viewValidation : Model -> Html msg
 viewValidation model =
-    let
-        (color, message) =
-            if model.password == model.passwordAgain then
-                ("green", "OK")
-            else
-                ("red", "Passwords do not match!")
-
-    in
-        div [style [("color", color)]] [ text message ]
+  let
+    (color, message) =
+      if not (model.submitted) then
+        ("green", "OK")
+      else if String.length model.password < 8 then
+        ("red", "Password must be at least 8 chars")
+      else if not (Regex.contains (Regex.regex "\\d") model.password) then
+        ("red", "Password must contain a digit")
+      else if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Passwords do not match!")
+  in
+    div [style [("color", color)]] [ text message ]
